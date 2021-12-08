011e4f232b5a8a82e66e0e83118b8bf0596960b7
ryusterakiba <ryusterakiba@berkeley.edu>
2021-12-07 16:09:27 -0800

    Velocity scaling on planned trajectory

--- src/motion/src/conductor_motion.py
+++ src/motion/src/conductor_motion.py
@@ -21,12 +21,6 @@ from conductor_planner import PathPlanner
 
 # ============================================================================ #
 
-MOTION_SPEED_1 = 1
-MOTION_SPEED_2 = .2
-MOTION_SPEED_3 = .8
-MOTION_SPEED_4 = 1
-
-
 file_location = '/home/cc/ee106a/fl21/class/ee106a-abw/ros_workspaces/robot_conductor_ryu/src/motion/positions/'
 
 # ============================================================================ #
@@ -36,20 +30,24 @@ Execution
 A way without using waypoints.
 Use joint positions
 '''
-def timed_joint_path(right_arm_motion,left_arm_motion,speed,time):
+def timed_joint_path(right_arm_motion,left_arm_motion,speed,tempo):
     while not rospy.is_shutdown():
-        planner = PathPlanner("both_arms",speed)
-
+        planner = PathPlanner("both_arms")
+        time = 60/tempo
         for i in range(len(right_arm_motion)):
             joint_goal_right = np.loadtxt(file_location + right_arm_motion[i]+ ".txt" ).tolist()
             joint_goal_left  = np.loadtxt(file_location + left_arm_motion[i]+ ".txt").tolist()
             joint_goal = joint_goal_left+joint_goal_right
             try:
-                #print(joint_goal)
+                #Plan
+                plan = planner.plan_to_joint_goal(joint_goal, speed)
+
+                #Execute
                 #raw_input("Press Enter to go")
-                planner._group.go(joint_goal,wait=True)
-                # rospy.sleep(time+.5)
-                # planner._group.stop()
+                planner.execute_plan(plan,timed = True)
+                rospy.sleep(time)
+                planner._group.stop()
+                
             except Exception as e:
                 print e
                 traceback.print_exc()
@@ -57,9 +55,8 @@ def timed_joint_path(right_arm_motion,left_arm_motion,speed,time):
 def go_to_joint_position(position):
     while not rospy.is_shutdown():
         try:
-            planner = PathPlanner("both_arms",1)
+            planner = PathPlanner("both_arms")
             joint_goal = np.loadtxt(file_location + position + ".txt").tolist()
-            
             planner._group.go(joint_goal,wait=True)
 
         except Exception as e:
@@ -73,15 +70,16 @@ def go_to_joint_position(position):
 def main():
     """
     Main Script
-    Assumes we started at initial config (position beat 3)
     """
     right_arm_motion = ["right_beat_1","right_beat_2","right_beat_3","right_beat_4","right_beat_1"]
     left_arm_motion  = ["left_neutral","left_neutral","left_crescendo_bot","left_crescendo_top","left_neutral"]
-    # right_arm_motion = ["right_beat_1"]
-    # left_arm_motion = ["left_crescendo_bot"]
 
+    #Initial Position
     go_to_joint_position("neutral_both")
-    timed_joint_path(right_arm_motion,left_arm_motion,1,3)
+
+    #Start
+    raw_input("Press Enter to go")
+    timed_joint_path(right_arm_motion,left_arm_motion,1,60)
 
 
 if __name__ == '__main__':
