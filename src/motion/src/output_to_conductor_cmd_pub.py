a3c06ade8e10837a99bdfddce253b4137e5d9bac
Class Account <ee106a-abw@c105-7.local>
2021-12-08 22:39:25 -0800

    new song

--- src/motion/src/output_to_conductor_cmd_pub.py
+++ src/motion/src/output_to_conductor_cmd_pub.py
@@ -39,6 +39,15 @@ left_arm_motion  = ["left_beat_1","left_beat_2","left_beat_3","left_beat_4"]
 right_arm_last_note_half = ["right_beat_1","right_beat_2","right_last_hold","right_last_swing","right_last_end"]
 left_arm_last_note_half = ["left_beat_1","left_beat_2","left_last_hold","left_last_swing","left_last_end"]
 
+# SPECIFIC TO LAST NOTE = whole NOTE
+right_arm_last_note_whole = ["right_last_hold","right_last_swing","right_last_end"]
+left_arm_last_note_whole = ["left_last_hold","left_last_swing","left_last_end"]
+
+#Crescendo over 4 beats
+left_arm_crescendo_rest = ["left_neutral"]*4
+left_arm_crescendo = ["left_crescendo_start","left_crescendo_1_4",
+					"left_crescendo_2_4","left_crescendo_3_4"]
+
 # ============================================================================ #
 
 def calc_num_measures(line):
@@ -63,9 +72,19 @@ def talker():
 	r = rospy.Rate(10)
 
 	while not rospy.is_shutdown():
-		message = raw_input("Enter song file: ")
-		music_file = file_location + music
+		message = raw_input("Enter song file (either hot_cross_buns or canon_in_d : ")
 		
+
+		if message == "hot_cross_buns":
+			music = 'hcb_theoretical_wo_time.txt'
+		elif message == "canon_in_d":
+			music = "canon_in_d_theoretical_wo_time.txt"
+		else:
+			print("Invalid song file!")
+			break
+
+		music_file = file_location + music
+
 		## Variable bookkeeping
 		total_num_measures = 0
 		last_note_duration = 0
@@ -79,20 +98,40 @@ def talker():
 		robot_commands = music_commands()
 		# Need to separate functionality later on (if more time). For now, both arms mirror each other up until the last measure
 		robot_commands.right_arm_motions = right_arm_motion * int(total_num_measures - 1)
-		robot_commands.left_arm_motions = left_arm_motion * int(total_num_measures - 1)
+		
+		print(total_num_measures)
+		print(last_note_duration)
+
+		if (last_note_duration == 2):
+			robot_commands.left_arm_motions = left_arm_motion * int(total_num_measures - 1)
+		elif (last_note_duration == 4):
+			robot_commands.left_arm_motions = left_arm_motion * int(total_num_measures - 3)
+			robot_commands.left_arm_motions.extend(left_arm_crescendo_rest)
+			robot_commands.left_arm_motions.extend(left_arm_crescendo)
 
 		# Last measure
 		if (last_note_duration == 2):
 			robot_commands.right_arm_motions.extend(right_arm_last_note_half)
 			robot_commands.left_arm_motions.extend(left_arm_last_note_half)
+		elif (last_note_duration == 4):
+			robot_commands.right_arm_motions.extend(right_arm_last_note_whole)
+			robot_commands.left_arm_motions.extend(left_arm_last_note_whole)
+
 
 		# Ending sequence
 		tempo = np.ones(len(robot_commands.right_arm_motions))
 
 		if (last_note_duration == 2):
 			#Cutoff actions, slower speed
+			tempo[-3:] = np.ones(3)*1.3
+		elif (last_note_duration == 4):
 			tempo[-3:] = np.ones(3)*1.5
 		robot_commands.tempo = tempo
+
+		print(len(robot_commands.tempo))
+		print(len(robot_commands.right_arm_motions))
+		print(len(robot_commands.left_arm_motions))
+
 
 		pub.publish(robot_commands)
 		print("Ready to run conductor_motion.py!")
